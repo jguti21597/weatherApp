1ad7b2197ce8fac5c562e89aaf948440548861cf
griffin1995 <jtgriffin95@gmail.com>
2024-03-04 15:19:49 +0000

    Used maps api 'size' object instead of standard x & y

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import {
   GoogleMap,
@@ -7,12 +7,10 @@ import {
   InfoWindow,
   DirectionsService,
   DirectionsRenderer,
-  Autocomplete as GoogleMapsAutocomplete
+  Autocomplete as GoogleMapsAutocomplete,
 } from "@react-google-maps/api";
 
-
-type MapOptions = google.maps.MapOptions;      //allows to import mapId/other options
-
+type MapOptions = google.maps.MapOptions; //allows to import mapId/other options
 
 const mapContainerStyle = {
   width: "100%",
@@ -27,9 +25,9 @@ const center = {
 function Intro() {
   const [origin, setOrigin] = useState("");
   const [destination, setDestination] = useState("");
-  const [directions, setDirections] = useState<google.maps.DirectionsResult | undefined>(
-    undefined
-  );
+  const [directions, setDirections] = useState<
+    google.maps.DirectionsResult | undefined
+  >(undefined);
 
   const mapRef = useRef<GoogleMap>(null);
 
@@ -39,12 +37,13 @@ function Intro() {
     }),
     []
   ) as any;
-  
+
   const [open, setOpen] = useState(false);
-  const [currentLocation, setCurrentLocation] = useState<google.maps.LatLng | null>(null);
+  const [currentLocation, setCurrentLocation] =
+    useState<google.maps.LatLng | null>(null);
 
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: "AIzaSyCeEkBOH3opYxGFCa43S9ZtBQ0OIm8LQr4",      
+    googleMapsApiKey: "AIzaSyCeEkBOH3opYxGFCa43S9ZtBQ0OIm8LQr4",
     libraries: ["places"],
   });
 
@@ -67,13 +66,17 @@ function Intro() {
     }
   }, [origin, destination]);
 
-
   //use geolocation API
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          setCurrentLocation(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+          setCurrentLocation(
+            new google.maps.LatLng(
+              position.coords.latitude,
+              position.coords.longitude
+            )
+          );
         },
         (error) => {
           console.log("Error getting location: ", error);
@@ -88,68 +91,83 @@ function Intro() {
   if (loadError) return <div>Error loading Google Maps API</div>;
   if (!isLoaded) return <div>Loading...</div>;
 
-  
-
   return (
     <div className="container">
       <div className="controls">
         <h1>Plan Route</h1>
         <div className="search">
-          <GoogleMapsAutocomplete >
-          <input type="text" placeholder="Origin" value={origin}
-          onChange={(e) => setOrigin(e.target.value)}/>
+          <GoogleMapsAutocomplete>
+            <input
+              type="text"
+              placeholder="Origin"
+              value={origin}
+              onChange={(e) => setOrigin(e.target.value)}
+            />
           </GoogleMapsAutocomplete>
           <GoogleMapsAutocomplete>
-          <input type="text" placeholder="Destination" value={destination}
-          onChange={(e) => setDestination(e.target.value)}/>
+            <input
+              type="text"
+              placeholder="Destination"
+              value={destination}
+              onChange={(e) => setDestination(e.target.value)}
+            />
           </GoogleMapsAutocomplete>
         </div>
         <div className="button">
-        <button id="clear" onClick={() => {
-          setOrigin("");
-          setDestination("");
-          setDirections(undefined);
-        }}>Clear</button>
+          <button
+            id="clear"
+            onClick={() => {
+              setOrigin("");
+              setDestination("");
+              setDirections(undefined);
+            }}
+          >
+            Clear
+          </button>
         </div>
       </div>
-      
+
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={9}
         center={currentLocation || center}
         options={options}
       >
-          {directions && (
-            <DirectionsRenderer
-              directions={directions}
-              options={{
-                polylineOptions: {
-                  zIndex: 50,
-                  strokeColor: "black",
-                  strokeWeight: 5,
-                },
-              }}
-            />
-          )}
-        <Marker
-          position={currentLocation || center}      //Places marker at current location 
-          icon={{                                   //marker is a bicylce icon 
-            url: "https://img.icons8.com/material/24/cycling-track.png",
-            scaledSize: { width: 40, height: 40 },
-          }}
-          onClick={() => setOpen(true)}
-        />
-        
-        {open && (                     //When click marker will display this message 
-          <InfoWindow position={currentLocation || center} onCloseClick={() => setOpen(false)}>      
-            <p>Your current location</p>        
+        {directions && (
+          <DirectionsRenderer
+            directions={directions}
+            options={{
+              polylineOptions: {
+                zIndex: 50,
+                strokeColor: "black",
+                strokeWeight: 5,
+              },
+            }}
+          />
+        )}
+        {isLoaded && (
+          <Marker
+            position={currentLocation || center} // Places marker at current location
+            icon={{
+              // marker is a bicycle icon
+              url: "https://img.icons8.com/material/24/cycling-track.png",
+              scaledSize: new google.maps.Size(40, 40), // Use google.maps.Size
+            }}
+            onClick={() => setOpen(true)}
+          />
+        )}
+
+        {open && ( //When click marker will display this message
+          <InfoWindow
+            position={currentLocation || center}
+            onCloseClick={() => setOpen(false)}
+          >
+            <p>Your current location</p>
           </InfoWindow>
         )}
       </GoogleMap>
-  </div>
+    </div>
   );
 }
 
 export default Intro;
-
- 
