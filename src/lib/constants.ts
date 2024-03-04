7813c8827270bf4807af4d278639f79493d0c577
griffin1995 <jtgriffin95@gmail.com>
2024-03-04 13:50:12 +0000

    hardlink api key

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,10 @@
 import { env } from "process"
 
 export const apiUrl = (location: string) => {
-    return `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${env.API_KEY}`
+    return `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=65ad34ef51cc68f1567d459fc99efa63`
     
 }
 
 export const apiUrlForecast = (location: string) => (
-    `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${env.API_KEY}`
+    `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=65ad34ef51cc68f1567d459fc99efa63`
 )
