0dbde0dd0f28d461ea16e1f6ae8d696871e38b7b
griffin1995 <jtgriffin95@gmail.com>
2024-03-11 15:25:59 +0000

    changed InfoCard svg's to white

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,19 +1,22 @@
-import { LucideIcon } from 'lucide-react'
-import React from 'react'
+import { LucideIcon } from "lucide-react";
+import React from "react";
 
 interface InfoCardProps {
-    value: number,
-    unit: string,
-    icon: LucideIcon
+  value: number;
+  unit: string;
+  icon: LucideIcon;
 }
 
-const InfoCard = ({value, unit, icon: Icon}: InfoCardProps) => {
+const InfoCard = ({ value, unit, icon: Icon }: InfoCardProps) => {
   return (
-    <div className=' flex flex-col justify-center items-center'>
-        <Icon strokeWidth={1.4} size={60}/>
-        <h1 className=' text-secondary font-bold text-2xl'>{value ? value : 0}{unit}</h1>
+    <div className=" flex flex-col justify-center items-center">
+      <Icon strokeWidth={1.4} size={60} color="white" />
+      <h1 className=" text-secondary font-bold text-2xl">
+        {value ? value : 0}
+        {unit}
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default InfoCard
+export default InfoCard;
