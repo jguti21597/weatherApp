dad801a5d6e5f592c6a5f15808b0dfe052862006
griffin1995 <jtgriffin95@gmail.com>
2024-03-11 11:38:59 +0000

    reponsive wind angle fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,10 +68,12 @@ export default async function Home() {
           </div>
           {angle > 0 ? (
             <div className="text-white flex flex-col justify-center items-center">
+              <div style={{transform: `rotate(${weatherdata.wind.deg}deg)`}}>
               <Navigation2
                 size={100}
-                className={`rotate-[${weatherdata.wind.deg}deg]`}
-              />{" "}
+                
+              />
+              </div>
               <h1 className="text-xl font-bold">{weatherdata.wind.deg}°</h1>
             </div>
           ) : (
