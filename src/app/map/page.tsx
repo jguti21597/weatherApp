548fff18b97d2872bed0b81c9636ba4115bf8420
ec22535 <ec22535@qmul.ac.uk>
2024-03-19 01:42:31 +0000

    map page update

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -5,12 +5,13 @@ import {
   useLoadScript,
   Marker,
   InfoWindow,
-  DirectionsService,
   DirectionsRenderer,
   Autocomplete as GoogleMapsAutocomplete,
 } from "@react-google-maps/api";
+import { apiUrl, apiUrlForecast } from "@/lib/constants";
+import { WeatherData, WeatherResponse } from "@/lib/types";
 
-type MapOptions = google.maps.MapOptions; //allows to import mapId/other options
+type MapOptions = google.maps.MapOptions;
 
 const mapContainerStyle = {
   width: "100%",
@@ -22,32 +23,35 @@ const center = {
   lng: 0.1276,
 };
 
-function Intro() {
+async function Intro() {
   const [origin, setOrigin] = useState("");
   const [destination, setDestination] = useState("");
-  const [directions, setDirections] = useState<
-    google.maps.DirectionsResult | undefined
-  >(undefined);
-
+  const [directions, setDirections] = useState<google.maps.DirectionsResult | undefined>(undefined);
   const mapRef = useRef<GoogleMap>(null);
+  const [open, setOpen] = useState(false);
+  const [currentLocation, setCurrentLocation] = useState<google.maps.LatLng | null>(null);
+
+  const LOCATION = "London";
+  const resp = await fetch(apiUrlForecast(LOCATION), { cache: "no-cache" });
+  const forecasts = (await resp.json()) as WeatherResponse;
+
+  const resp2 = await fetch(apiUrl("London"), { cache: "no-cache" });
+  const weatherdata = (await resp2.json()) as WeatherData;
+
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyBX1qw0tVyzKSvIhtdW-R9103FZTvU44Xs", // Replace with your Google Maps API key
+    libraries: ["places"],
+  });
 
   const options = useMemo<MapOptions>(
     () => ({
       mapId: "62edfa1f0518c7bd",
     }),
     []
-  ) as any;
+  );
 
-  const [open, setOpen] = useState(false);
-  const [currentLocation, setCurrentLocation] =
-    useState<google.maps.LatLng | null>(null);
 
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: "",
-    libraries: ["places"],
-  });
-
-  //use directions API
   useEffect(() => {
     if (origin && destination) {
       const directionsService = new google.maps.DirectionsService();
@@ -66,17 +70,11 @@ function Intro() {
     }
   }, [origin, destination]);
 
-  //use geolocation API
   useEffect(() => {
-    if (navigator.geolocation) {
+    if (isLoaded && navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          setCurrentLocation(
-            new google.maps.LatLng(
-              position.coords.latitude,
-              position.coords.longitude
-            )
-          );
+          setCurrentLocation(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
         },
         (error) => {
           console.log("Error getting location: ", error);
@@ -85,9 +83,8 @@ function Intro() {
     } else {
       console.log("Geolocation not supported");
     }
-  }, []);
+  }, [isLoaded]);
 
-  //If theres problems loading the map/API
   if (loadError) return <div>Error loading Google Maps API</div>;
   if (!isLoaded) return <div>Loading...</div>;
 
@@ -147,22 +144,30 @@ function Intro() {
         )}
         {isLoaded && (
           <Marker
-            position={currentLocation || center} // Places marker at current location
+            position={currentLocation || center}
             icon={{
-              // marker is a bicycle icon
               url: "https://img.icons8.com/material/24/cycling-track.png",
-              scaledSize: new google.maps.Size(40, 40), // Use google.maps.Size
+              scaledSize: new google.maps.Size(40, 40),
             }}
             onClick={() => setOpen(true)}
           />
         )}
 
-        {open && ( //When click marker will display this message
+        {open && (
           <InfoWindow
             position={currentLocation || center}
             onCloseClick={() => setOpen(false)}
           >
-            <p>Your current location</p>
+            <p>hello</p>
+            <p>Your current location is </p>
+            <p>Temperature: {parseFloat((weatherdata.main.temp - 273).toFixed(0))} K</p>
+            {/* Display weather data */}
+            {weatherdata && (
+              <>
+                <p>Temperature: {parseFloat((weatherdata.main.temp - 273).toFixed(0))} K</p>
+                <p>Weather: {weatherdata.weather[0].description}</p>
+              </>
+            )}
           </InfoWindow>
         )}
       </GoogleMap>
